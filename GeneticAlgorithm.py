0c1c90fb0e1488c4e6220e4a73e2c05d0c753b78
Daniel <d.pekarmgci@gmail.com>
2018-08-07 18:37:18 -0400

    i forget

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -28,9 +28,6 @@ class Chromosome:
         for gene in self.genes:
             gene.generate_initial_values(ws)
 
-    #def run_sap_model
-        #should change self.fitness into fabi
-
     def mutation_uniform(self,prob):
         for i in range(self.len):
             if random.random()<prob:
@@ -172,39 +169,3 @@ class Population:
             for j in range(self.chromlen):
                 i.append(random.uniform(parents[0][j],parents[1][j]))
         return children
-
-wb = load_workbook('Setup.xlsx')
-ws = wb.active
-glen = (ws['J2']).value
-generations = 10
-max_fit = []
-avg_fit = []
-
-Pop = Population(chromosomelen = glen,pop = 10)
-Pop.create_initial_pop(ws)
-
-for i in range(generations):
-    for chromosome in Pop.chromosomes:
-        chromosome.run_sap_models()
-    avg_fit.append(Pop.avg_fitness())
-    max_fit.append(Pop.max_fitness())
-    new_Pop = Population(chromosomelen = glen, pop = 10)
-    temp_parent = Pop.selection_elitism(2) #[[gene1],[gene2]]
-    temp_parent_2 = Pop.selection_roulette(2)
-    temp_parents = temp_parent+temp_parent_2
-    parents = []
-    for j in temp_parents:
-        parents.append(j)
-    #use choices or something to get 5 pairs out of 4 parents
-    #apply same crossover for all pairs of parents to get children
-    #put all children into the population
-    #for all choromosomes in population, mutate
-
-
-    #create sap models
-    #eval max and avg fitness
-    #apply elitism for 2 best
-    #apply roulette/stochastic/rank/tournament selection
-    #choose 2 parents
-    #make 5 pairs of children using crossovers (npoint/randomflip/flip/uniform/triangular)
-    #mutate
